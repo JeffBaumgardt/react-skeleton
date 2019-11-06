248ac340c4741a5e5f1804b188b25b42442961b6
Jeff Baumgardt <medros1455@gmail.com>
2019-11-05 21:26:41 -0700

    Some eslint fixes for TS, like no-undef and prop-type warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,37 @@
 module.exports = {
-	extends: ['react-app', './node_modules/react-tools/eslint.js'],
-	rules: {
-		'react/prop-types': 'warn',
-		'react/no-did-mount-set-state': 'off',
-		'import/no-named-as-default': 'off',
-		'import/no-unresolved': 'off',
-		'import/extensions': [
-			'error',
-			{
-				jpg: 'always',
-				png: 'always',
-				js: 'never',
-				jsx: 'never',
-				ts: 'never',
-				tsx: 'never',
-				json: 'always',
-				JPG: 'always',
-			},
-		],
-		'jsx-a11y/click-events-have-key-events': 'off',
-		'no-return-await': 'off',
-		'no-shadow': 'off',
-		'no-console': 'warn',
-		'jest/prefer-called-with': 'warn',
-		'jest/valid-expect-in-promise': 'warn',
+  extends: ['react-app', './node_modules/react-tools/eslint.js'],
+  rules: {
+    'react/prop-types': 'off', // we have TS!
+    'react/no-did-mount-set-state': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': [
+      'error',
+      {
+        jpg: 'always',
+        png: 'always',
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        json: 'always',
+        JPG: 'always',
+      },
+    ],
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'no-return-await': 'off',
+    'no-shadow': 'off',
+    'no-console': 'warn',
+    'jest/prefer-called-with': 'warn',
+    'jest/valid-expect-in-promise': 'warn',
+    '@typescript-eslint/no-var-requires': 'off',
 	},
+	overrides: [
+		{
+			files: ['**/*.ts?(x)'],
+			rules: {
+				'no-undef': 'off' // eslint fires no-undef in type annotations, bad
+			}
+		}
+	]
 }
