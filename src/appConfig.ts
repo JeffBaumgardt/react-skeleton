0642aca432442cd16581494edfb02e1150e60af2
Jeff Baumgardt <medros1455@gmail.com>
2019-11-06 11:13:22 -0700

    Pull app name from env.

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,13 +1,13 @@
 export interface AppConfig {
-	API_ROOT: string;
-	APP_NAME: string;
+    API_ROOT: string
+    APP_NAME: string
 }
 
 export const API_ROOT = process.env.REACT_APP_API_ROOT || ''
 
-export const APP_NAME = '' // Application name goes here. This is just used as a key for project scoping
+export const APP_NAME = process.env.REACT_APP_APP_NAME || '' // Application name goes here. This is just used as a key for project scoping
 
 export default {
-	API_ROOT,
-	APP_NAME,
+    API_ROOT,
+    APP_NAME,
 }
