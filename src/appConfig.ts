b78c8777c30225083acbddd1bc6a148d52623714
Jeff Baumgardt <medros1455@gmail.com>
2019-11-06 10:46:42 -0700

    Add an app name key, this probably will come from the env config, we'll see.

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,9 +1,13 @@
 export interface AppConfig {
-    API_ROOT: string;
+	API_ROOT: string;
+	APP_NAME: string;
 }
 
 export const API_ROOT = process.env.REACT_APP_API_ROOT || ''
 
+export const APP_NAME = '' // Application name goes here. This is just used as a key for project scoping
+
 export default {
-    API_ROOT,
+	API_ROOT,
+	APP_NAME,
 }
